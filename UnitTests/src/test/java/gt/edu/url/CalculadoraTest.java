7c59207e842dc95b34ae489fa1c7c50fb56afec8
José Girón <42883411+Jocagi@users.noreply.github.com>
2021-03-08 00:09:13 -0600

    Pruebas unitarias: Video 2 - parte 1

--- UnitTests/src/test/java/gt/edu/url/CalculadoraTest.java
+++ UnitTests/src/test/java/gt/edu/url/CalculadoraTest.java
@@ -1,19 +1,56 @@
 package gt.edu.url;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 public class CalculadoraTest
 {
+    Calculadora calculadora;
+
+    @BeforeAll
+    public static void startup()
+    {
+        System.out.println("Inicializador global");
+    }
+
+    @BeforeEach
+    public void init()
+    {
+        calculadora = new Calculadora();
+        System.out.println("Inicializando test");
+    }
+
     @Test
     public void probarSuma()
     {
-        //Esperado
-        double esperado = 25.0;
-        //Ejecutar y obtener
-        var calculadora = new Calculadora();
-        double obtenido = calculadora.sumar(5.0, 20.0);
-        //Comparar esperado vs obtenido
-        assertEquals(esperado, obtenido);
+        System.out.println("Sumando...");
+        assertEquals(25.0, calculadora.sumar(5.0, 20.0));
+    }
+
+    @Test
+    public void probarResta()
+    {
+        System.out.println("Restando...");
+        assertEquals(30.0, calculadora.restar(50, 20.0));
+    }
+
+    @Test
+    public void probarMultiplicacion()
+    {
+        System.out.println("Multiplicando...");
+        assertEquals(25.0, calculadora.multiplicar(5.0, 5.0));
+    }
+
+    @AfterEach
+    public void shutdown()
+    {
+        System.out.println("Finalizando test");
+    }
+
+    @AfterAll
+    public static void destroy()
+    {
+        System.out.println("Finalizacion global");
     }
 }
